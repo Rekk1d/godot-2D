3594ef85978d64cfb47c09de1479a947c15eb124
Rekk1d <xxxdaddy420@gmail.com>
2025-12-11 23:27:11 +0700

    add state machine in player

--- Player/player.gd
+++ Player/player.gd
@@ -9,6 +9,9 @@ enum {
 	SLIDE
 }
 
+@onready var animation = $AnimatedSprite2D
+@onready var animationPlayer = $AnimationPlayer
+
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
@@ -20,44 +23,38 @@ var run_speed = 1
 var is_slide_available = true
 var slide_cooldown = 1
 var is_sliding = false
-@onready var animationPlayer = $AnimatedSprite2D
+var combo = false
+var attack_cooldown = false
 
 func _physics_process(delta: float) -> void:
 	match state:
 		MOVE:
 			move_state()
 		ATTACK:
-			pass
+			attack_state()
 		ATTACK2:
-			pass
+			attack2_state()
 		ATTACK3: 
-			pass
+			attack3_state()
 		BLOCK:
 			block_state()
 		SLIDE:
 			slide_state()
-	# Add the gravity.
+			
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("attack") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-		#animation.play('Jump')
-
 		
 	if velocity.y > 0: 
-		animationPlayer.play("Fall")
+		play_anim('Fall')
 		
 	if health <= 0:
-		health = 0
-		animationPlayer.play("Death")
-		await animationPlayer.animation_finished
-		queue_free()
-		get_tree().change_scene_to_file("res://menu.tscn")
+		die()
 
 	move_and_slide()
 
+func play_anim(name: String):
+	if animationPlayer.current_animation != name:
+		animationPlayer.play(name)
 
 func move_state():
 	var direction:= Input.get_axis("left", "right")
@@ -72,16 +69,16 @@ func move_state():
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0: 
-			animationPlayer.play('Idle')
+			play_anim('Idle')
 	if Input.is_action_pressed('run'):
 		run_speed = 2
 	else:
 		run_speed = 1
 			
 	if direction == -1:
-		animationPlayer.flip_h = true
+		animation.flip_h = true
 	elif direction == 1:
-		animationPlayer.flip_h = false
+		animation.flip_h = false
 		
 	if Input.is_action_just_pressed("block"):
 		if(velocity.x == 0):
@@ -90,17 +87,53 @@ func move_state():
 			if is_slide_available:
 				state = SLIDE
 				is_slide_available = false
+	if Input.is_action_just_pressed("attack") and not attack_cooldown:
+		state = ATTACK
 	
 		
 func block_state():
 	velocity.x = 0
-	animationPlayer.play("Block")
+	play_anim('Block')
 	if Input.is_action_just_released("block"):
 		state = MOVE
 		
 func slide_state():
-	animationPlayer.play("Slide")
+	play_anim('Slide')
 	#await animationPlayer.animation_finished
 	await get_tree().create_timer(slide_cooldown).timeout
 	is_slide_available = true
 	state = MOVE
+
+func attack_state():
+	if Input.is_action_just_pressed("attack") and combo:
+		state = ATTACK2
+	velocity.x = 0
+	play_anim('Attack')
+	await animationPlayer.animation_finished
+	attack_freeze()
+	state = MOVE
+	
+func attack2_state():
+	if Input.is_action_just_pressed("attack") and combo:
+		state = ATTACK3
+	play_anim('Attack2')
+	await animationPlayer.animation_finished
+	state = MOVE
+	
+func attack3_state():
+	play_anim('Attack3')
+	await animationPlayer.animation_finished
+	state = MOVE
+	
+func attack_freeze():
+	attack_cooldown = true
+	get_tree().create_timer(0.5).timeout
+	attack_cooldown = false
+	
+func die():
+	health = 0
+	play_anim("Death")
+	await animationPlayer.animation_finished
+	queue_free()
+	get_tree().change_scene_to_file("res://menu.tscn")
+	
