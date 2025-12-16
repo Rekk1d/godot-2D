0bcc08f5b55ba8f5a1e4cf7177999d0ed579aa47
Rekk1d <xxxdaddy420@gmail.com>
2025-12-16 23:16:02 +0700

    refactoring, add mushoorms mob

---
+++ scenes/Levels/level.gd
@@ -0,0 +1,58 @@
+extends Node2D
+
+@onready var light = $Light/DirectionalLight2D
+@onready var lamp = $Light/PointLight2D
+@onready var dayCount = $CanvasLayer/DayCount
+@onready var animationPlayer = $CanvasLayer/AnimationPlayer
+
+enum {
+	MORNING,
+	DAY,
+	EVENING,
+	NIGHT
+}
+
+var state = MORNING
+var currentDay: int = 1
+
+func _ready(): 
+	light.enabled = true
+	setDayCount()
+	setDayAnimation()
+	
+func change_time(energy: float, time: int) -> void:
+	var dayTween = get_tree().create_tween(); 
+	dayTween.tween_property(light, "energy", energy, time)
+	
+func change_pointLight(energy: int) -> void:
+	var pointLight = get_tree().create_tween(); 
+	pointLight.tween_property(lamp, "energy", energy, 20)
+
+func _on_change_time_of_day_timeout() -> void:
+	match state:
+		MORNING:
+			change_time(0.2, 20)
+			change_pointLight(0)
+		#DAY:
+			#change_time(0.4, 60)
+		EVENING:
+			change_time(0.75, 20)
+			change_pointLight(1)
+		#NIGHT:
+			#change_time(0.9, 20)
+			
+	if state < 3:
+		state += 1
+	else:
+		state = 0
+		currentDay += 1
+		setDayCount()
+		setDayAnimation()
+		
+func setDayCount() -> void:
+	dayCount.text = "Day: " + str(currentDay)
+	
+func setDayAnimation(): 
+	animationPlayer.play("day_count_fadeIn")
+	await get_tree().create_timer(3).timeout
+	animationPlayer.play("day_count_fadeOut")
---
+++ scenes/Mobs/mushroom.gd
@@ -0,0 +1,70 @@
+extends CharacterBody2D
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var collisionShape = $AttackDirection/AttackRange/CollisionShape2D
+@onready var sprite = $AnimatedSprite2D
+@onready var attackDirection = $AttackDirection
+
+enum {
+	IDLE,
+	ATTACK,
+	CHASE
+}
+
+var player
+var direction
+var _state: int = IDLE
+
+var state: int:
+	set(value):
+		if _state == value:
+			return
+		_state = value
+		match _state:
+			IDLE:
+				idle_state()
+			ATTACK:
+				attack_state()
+			CHASE:
+				chase_state
+	get:
+		return _state
+	
+func _ready() -> void:
+	if Signals.has_signal("player_position_update"):
+		Signals.connect("player_position_update", Callable (self, "_on_player_position_update"))
+	
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if state == CHASE:
+		chase_state()
+	move_and_slide()
+		
+
+func _on_attack_range_body_entered(body: Node2D) -> void:
+	state = ATTACK
+	
+func _on_player_position_update(player_pos) -> void:
+		player = player_pos
+		
+func idle_state() -> void:
+	animationPlayer.play("Idle")
+	await get_tree().create_timer(1).timeout
+	collisionShape.disabled = false
+	state = CHASE
+	
+func attack_state() -> void:
+	animationPlayer.play("Attack")
+	await animationPlayer.animation_finished
+	collisionShape.disabled = true
+	state = IDLE
+	
+func chase_state() -> void:
+	direction = (player - self.position).normalized()
+	if direction.x < 0:
+		sprite.flip_h = true
+		attackDirection.rotation_degrees = 180
+	else:
+		sprite.flip_h = false
+		attackDirection.rotation_degrees = 180
